06f525a36dcb1250f6c18f06538fa54efd0d1f2f
Xolani Sibisi <xolansibisi@gmail.com>
2023-06-26 12:12:33 +0200

    Adding functionality

--- index.js
+++ index.js
@@ -10,10 +10,6 @@ setTimeout(function(){
 
 const theMessageButton = document.querySelector('.theMessageButton');
 theMessageButton.addEventListener('click', function() {
-    setTimeout(function(){
-        myMessage.innerText = 'This is a message in the DOM!'
-    }, 3000);
-    
     if (inputBox.value.trim().length > 0) {
         myMessage.innerText = inputBox.value;
      }
@@ -24,6 +20,10 @@ theClearBtn.addEventListener("click", function() {
     myMessage.innerHTML = "";
 });
 
+setTimeout(function(){
+    myMessage.innerText = '';
+}, 6000);
+
 
 myMessage.addEventListener('click', function() {
     myMessage.classList.toggle('darkmode')
@@ -32,7 +32,7 @@ myMessage.addEventListener('click', function() {
 const fruits = ['Apples', 'Pears', 'Oranges', 'Grapes', 'Bananas'];
 
 const fruitList = document.querySelector(".fruits");
-const inp = document.querySelector(".inputValue").value;
+const inp = document.querySelector(".inputValue");
 
 for(let i=0;i<fruits.length;i++){
    // get fruits from the list
@@ -40,12 +40,13 @@ for(let i=0;i<fruits.length;i++){
    
    // create a new li element
    const li = document.createElement('li');
-   const txt = document.createTextNode(inp);
    li.innerText = fruit;
    fruitList.appendChild(li);
 }
  const addBtn = document.querySelector(".addBtn");
  addBtn.addEventListener("click", function(){
-    li.innerHTML = inp.value;
-    fruitList.appendChild(txt);
+    const fruit = inp.value
+    const li = document.createElement('li');
+    li.innerHTML = fruit;
+    fruitList.appendChild(li);
  })
